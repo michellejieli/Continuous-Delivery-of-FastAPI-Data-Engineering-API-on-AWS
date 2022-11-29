e0c1574ca5833d1ef4ef15cdf826dec7196d3800
Michelle Li <70456530+michellejieli@users.noreply.github.com>
2022-11-29 06:44:27 +0000

    add test file

--- test_local.py
+++ test_local.py
@@ -2,11 +2,11 @@ import requests
 
 url = "https://yummly2.p.rapidapi.com/feeds/auto-complete"
 
-querystring = {"q":"cookie"}
+querystring = {"q": "tea"}
 
 headers = {
-	"X-RapidAPI-Key": "bd3adcf9d4msh6039b415026aa9dp14de35jsn843f232228bb",
-	"X-RapidAPI-Host": "yummly2.p.rapidapi.com"
+    "X-RapidAPI-Key": "bd3adcf9d4msh6039b415026aa9dp14de35jsn843f232228bb",
+    "X-RapidAPI-Host": "yummly2.p.rapidapi.com",
 }
 
 response = requests.request("GET", url, headers=headers, params=querystring, timeout=5)
